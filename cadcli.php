26488cac7b2afdbcd16e5bf2d0d6505fe342964a
carloskdu <carloskdu@itepbrasil.net>
2024-03-28 18:07:18 -0300

    testando chegado json

--- cadcli.php
+++ cadcli.php
@@ -10,6 +10,21 @@ require_once 'Cliente.php';
 $clienteNome = $_POST['clienteNome'];
 $clienteEmail = $_POST['clienteEmail'];
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['CONTENT_TYPE'] === 'application/json') {
+    // Recebe os dados JSON da requisição
+    $jsonData = file_get_contents('php://input');
+
+    // Converte os dados JSON em um array associativo ou objeto PHP
+    $data = json_decode($jsonData, true); // Se o segundo parâmetro for true, ele retornará um array associativo
+
+    // Verifica se a conversão foi bem-sucedida
+    if ($data !== null) {
+        // Agora você pode acessar os campos do array associativo e realizar o tratamento necessário
+        $campo1 = $data['campo1'];
+        $campo2 = $data['campo2'];
+
+    }
+}
 #$dotenv = Dotenv::createImmutable(__DIR__);
 #$dotenv->load();
 
@@ -47,7 +62,7 @@ $param = array(
 
 //$r = $kinghost->addClientes($param);
 
-var_dump($_POST);
+var_dump($data);
 
 //var_dump($param);
 ?>
